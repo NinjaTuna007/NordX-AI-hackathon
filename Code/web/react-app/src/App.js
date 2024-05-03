b86756ddb931fa4b018ed64753f68c99d2620e0c
agrover112 <agrover112@gmail.com>
2024-05-03 12:16:38 +0200

    feat: First working UI. Pages,Comp added

--- Code/web/react-app/src/App.js
+++ Code/web/react-app/src/App.js
@@ -1,12 +1,73 @@
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+
 import React, { useState } from 'react';
 import TextComparisonComponent from './TextComparisonComponent';
+import MacBookPro141 from "./pages/MacBookPro141";
+import GuideView from "./pages/GuideView";
+import ChangedView from "./pages/ChangedView";
+import ChangeView from "./pages/ChangeView";
 
 function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/guideview":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/changedview":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/changeview":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div>
-      <TextComparisonComponent />
-    </div>
+    <Routes>
+      <Route path="/" element={<MacBookPro141 />} />
+      <Route path="/guideview" element={<GuideView />} />
+      <Route path="/changedview" element={<ChangedView />} />
+      <Route path="/changeview" element={<ChangeView />} />
+    </Routes>
   );
 }
-
 export default App;
---
+++ Code/web/react-app/src/components/Result.jsx
@@ -0,0 +1,24 @@
+const Result = () => {
+  return (
+    <div className="self-stretch bg-darkblue flex flex-row items-start justify-between pt-[18.3px] pb-[15.3px] pr-[47px] pl-3 box-border max-w-full gap-[20px] z-[1] text-left text-5xl text-white font-inter mq450:flex-wrap mq1150:pr-[23px] mq1150:box-border">
+      <img
+        className="h-[63.9px] w-[863px] relative hidden max-w-full"
+        alt=""
+        src="/rectangle-11.svg"
+      />
+      <div className="w-[268px] flex flex-row items-start justify-start gap-[3px]">
+        <img
+          className="h-[25.2px] w-[38px] relative z-[1]"
+          alt=""
+          src="/chevronright.svg"
+        />
+        <div className="flex-1 relative z-[1] mq450:text-lgi">Section-1</div>
+      </div>
+      <div className="h-[16.1px] relative text-orange inline-block min-w-[51px] z-[1] mq450:text-lgi">
+        99%
+      </div>
+    </div>
+  );
+};
+
+export default Result;
